1f099ac77d9cad004ebe30b5a68c1f05252e18a8
Andreas Kloeckner <inform@tiker.net>
2009-02-19 01:31:04 -0500

    Refactor local-dt into test rig class.

--- local-dt/advection.py
+++ local-dt/advection.py
@@ -24,27 +24,16 @@ import numpy.linalg as la
 
 
 
-def main() :
-    from math import sin, cos, pi, sqrt
-    from math import floor
+class LocalDtTestRig:
+    def __init__(self):
+        self.make_mesh_1d()
+        self.make_operator()
+        self.make_discretization()
+        self.make_rhss()
+        self.make_timestepper()
 
-    def f(x):
-        return sin(pi*x)
-
-    def u_analytic(x, el, t):
-        return f((-numpy.dot(v, x)/norm_v+t*norm_v))
-
-    def boundary_tagger(vertices, el, face_nr):
-        if numpy.dot(el.face_normals[face_nr], v) < 0:
-            return ["inflow"]
-        else:
-            return ["outflow"]
-
-    dim = 1
-
-    if dim == 1:
+    def make_mesh_1d(self):
         transition_point = 10
-        v = numpy.array([1])
 
         def el_tagger(el, all_vertices):
             if el.centroid(all_vertices)[0] < transition_point:
@@ -52,159 +41,157 @@ def main() :
             else:
                 return ["large"]
 
-        from hedge.mesh import make_1d_mesh
+        def boundary_tagger(vertices, el, face_nr):
+            if numpy.dot(el.face_normals[face_nr], v) < 0:
+                return ["inflow"]
+            else:
+                return ["outflow"]
+
         eps = 1e-5
 
-        mesh = make_1d_mesh(
+        from hedge.mesh import make_1d_mesh
+        self.mesh = make_1d_mesh(
                 numpy.hstack((
                     numpy.arange(0, transition_point, 0.1),
                     numpy.arange(transition_point, 20+eps, 0.2),
                     )), 
                 periodic=True,
                 element_tagger=el_tagger)
-    elif dim == 2:
-        v = numpy.array([2,0])
-        from hedge.mesh import make_disk_mesh
-        mesh = make_disk_mesh(boundary_tagger=boundary_tagger)
-    elif dim == 3:
-        v = numpy.array([0,0,1])
-        from hedge.mesh import make_cylinder_mesh, make_ball_mesh, make_box_mesh
 
-        mesh = make_cylinder_mesh(max_volume=0.04, height=2, 
-                boundary_tagger=boundary_tagger,
-                periodic=False, radial_subdivisions=32)
-    else:
-        raise RuntimeError, "bad number of dimensions"
+        from hedge.partition import partition_from_tags, partition_mesh
+        self.small_part, self.large_part = partition_mesh(self.mesh, 
+                partition_from_tags(self.mesh, {"large": 1}),
+                part_bdry_tag_factory=lambda opp_part:
+                "from_large" if opp_part == 1 else "from_small")
 
-    norm_v = la.norm(v)
+    def make_operator(self):
+        self.v = numpy.array([1])
+        self.norm_v = la.norm(self.v)
 
-    from hedge.partition import partition_from_tags, partition_mesh
-    small_part, large_part = partition_mesh(mesh, 
-            partition_from_tags(mesh, {"large": 1}),
-            part_bdry_tag_factory=lambda opp_part:
-            "from_large" if opp_part == 1 else "from_small")
+        from hedge.data import \
+                ConstantGivenFunction, \
+                TimeConstantGivenFunction, \
+                TimeDependentGivenFunction
+        from hedge.pde import StrongAdvectionOperator, WeakAdvectionOperator
+        self.op = WeakAdvectionOperator(self.v, 
+                flux_type="upwind")
 
-    from hedge.backends.jit import Discretization
-    order = 4
-    small_discr = Discretization(small_part.mesh, order=order, debug=["node_permutation"])
-    large_discr = Discretization(large_part.mesh, order=order, debug=["node_permutation"])
-    whole_discr = Discretization(mesh, order=order)
+    def make_discretization(self, order=4):
+        from hedge.backends.jit import Discretization
+        self.small_discr = Discretization(self.small_part.mesh, 
+                order=order, debug=["node_permutation"])
+        self.large_discr = Discretization(self.large_part.mesh, 
+                order=order, debug=["node_permutation"])
+        self.whole_discr = Discretization(self.mesh, order=order)
 
-    from hedge.visualization import VtkVisualizer, SiloVisualizer
-    #vis = VtkVisualizer(vis_discr, rcon, "fld")
-    vis = SiloVisualizer(whole_discr)
+        from functools import partial
+        from hedge.partition import reassemble_parts
+        self.reassemble = partial(reassemble_parts,
+                self.whole_discr, 
+                [self.small_part, self.large_part],
+                [self.small_discr, self.large_discr])
 
-    # operator setup ----------------------------------------------------------
-    from hedge.data import \
-            ConstantGivenFunction, \
-            TimeConstantGivenFunction, \
-            TimeDependentGivenFunction
-    from hedge.pde import StrongAdvectionOperator, WeakAdvectionOperator
-    op = WeakAdvectionOperator(v, 
-            inflow_u=TimeDependentGivenFunction(u_analytic),
-            flux_type="upwind")
+        from hedge.visualization import SiloVisualizer
+        self.vis = SiloVisualizer(self.whole_discr)
 
-    large_u = large_discr.interpolate_volume_function(lambda x, el: u_analytic(x, el, 0))
-    small_u = small_discr.interpolate_volume_function(lambda x, el: u_analytic(x, el, 0))
+    def make_timestepper(self):
+        large_dt = self.large_discr.dt_factor(self.op.max_eigenvalue())
+        small_dt = self.small_discr.dt_factor(self.op.max_eigenvalue())
 
-    # timestep setup ----------------------------------------------------------
+        assert small_dt >= large_dt/2
 
-    large_dt = large_discr.dt_factor(op.max_eigenvalue())
-    small_dt = small_discr.dt_factor(op.max_eigenvalue())
-    nsteps = int(700/max(large_dt, small_dt))
+        from hedge.timestep import TwoRateAdamsBashforthTimeStepper
+        self.stepper = TwoRateAdamsBashforthTimeStepper(
+                large_dt=large_dt*0.025, step_ratio=2, 
+                order=1)
 
-    assert small_dt >= large_dt/2
+    def make_rhss(self):
+        self.rhs = self.op.bind(self.whole_discr)
+        small_rhs = self.op.bind(self.small_discr)
+        large_rhs = self.op.bind(self.large_discr)
 
-    from hedge.timestep import TwoRateAdamsBashforthTimeStepper
-    stepper = TwoRateAdamsBashforthTimeStepper(
-            large_dt=large_dt*0.025, step_ratio=2, 
-            order=1)
-    print "large dt=%g, small_dt=%g, nsteps=%d" % (stepper.large_dt, stepper.small_dt, nsteps)
+        large_to_small_rhs = self.op.bind_interdomain(
+                self.small_discr, self.small_part,
+                self.large_discr, self.large_part,
+                )
+        small_to_large_rhs = self.op.bind_interdomain(
+                self.large_discr, self.large_part,
+                self.small_discr, self.small_part,
+                )
 
-    # diagnostics setup -------------------------------------------------------
-    from pytools.log import LogManager, \
-            add_general_quantities, \
-            add_simulation_quantities, \
-            add_run_info
+        def full_rhs_small(t, u_small, u_large): 
+            return small_rhs(t, u_small)
+        def full_rhs_l2s(t, u_small, u_large): 
+            return large_to_small_rhs(t, u_small, u_large)
+        def full_rhs_large(t, u_small, u_large): 
+            return large_rhs(t, u_large)
+        def full_rhs_s2l(t, u_small, u_large):
+            return small_to_large_rhs(t, u_large, u_small)
 
-    logmgr = LogManager("advection.dat", "w")
-    add_run_info(logmgr)
-    add_general_quantities(logmgr)
-    add_simulation_quantities(logmgr, stepper.large_dt)
+        self.rhss = [full_rhs_small, full_rhs_l2s,
+                full_rhs_s2l, full_rhs_large]
 
-    logmgr.add_watches(["step.max", "t_step.max"])
+        def reassembled_rhs(t, u):
+            return self.reassemble([
+                full_rhs_small(t, *u)+full_rhs_l2s(t, *u),
+                full_rhs_s2l(t, *u)+full_rhs_large(t, *u),
+                ])
 
-    # timestep loop -----------------------------------------------------------
-    rhs = op.bind(whole_discr)
-    small_rhs = op.bind(small_discr)
-    large_rhs = op.bind(large_discr)
+        self.reassembled_rhs = reassembled_rhs
 
-    large_to_small_rhs = op.bind_interdomain(
-            small_discr, small_part,
-            large_discr, large_part,
-            )
-    small_to_large_rhs = op.bind_interdomain(
-            large_discr, large_part,
-            small_discr, small_part,
-            )
+    def do_timestep(self):
+        dt = self.stepper.large_dt
+        nsteps = int(700/dt)
 
-    def full_rhs_small(t, u_small, u_large): 
-        return small_rhs(t, u_small)
-    def full_rhs_l2s(t, u_small, u_large): 
-        return large_to_small_rhs(t, u_small, u_large)
-    def full_rhs_large(t, u_small, u_large): 
-        return large_rhs(t, u_large)
-    def full_rhs_s2l(t, u_small, u_large):
-        return small_to_large_rhs(t, u_large, u_small)
+        print "large dt=%g, small_dt=%g, nsteps=%d" % (
+                self.stepper.large_dt, self.stepper.small_dt, nsteps)
 
-    rhss = [full_rhs_small, full_rhs_l2s,
-            full_rhs_s2l, full_rhs_large]
+        from math import sin, cos, pi, sqrt
 
-    from functools import partial
-    from hedge.partition import reassemble_parts
-    reassemble = partial(reassemble_parts,
-            whole_discr, 
-            [small_part, large_part],
-            [small_discr, large_discr])
+        def f(x):
+            return sin(pi*x)
 
-    from hedge.tools import make_obj_array
-    u = [small_u, large_u]
+        def u_analytic(x, el, t):
+            return f((-numpy.dot(self.v, x)/self.norm_v+t*self.norm_v))
 
-    def reassembled_rhs(t, u):
-        return reassemble([
-            full_rhs_small(t, *u)+full_rhs_l2s(t, *u),
-            full_rhs_s2l(t, *u)+full_rhs_large(t, *u),
-            ])
+        large_u = self.large_discr.interpolate_volume_function(
+                lambda x, el: u_analytic(x, el, 0))
+        small_u = self.small_discr.interpolate_volume_function(
+                lambda x, el: u_analytic(x, el, 0))
 
-    for step in xrange(nsteps):
-        logmgr.tick()
+        u = [small_u, large_u]
 
-        t = step*stepper.large_dt
+        for step in xrange(nsteps):
+            if step % 100 == 0:
+                print step, la.norm(u[0]), la.norm(u[1])
 
-        if step % 10 == 0:
-            whole_u = reassemble(u)
-            u_rhs_real = rhs(t, whole_u)
+            t = step*dt
 
-            visf = vis.make_file("fld-%04d" % step)
-            u_rhs = reassembled_rhs(t, u)
-            vis.add_data(visf, [ 
-                ("u", whole_u), 
-                ("u_rhs", u_rhs), 
-                ("u_rhs_real", u_rhs_real), 
-                ("rhsdiff", u_rhs_real-u_rhs), 
-                ], 
-                time=t, step=step)
-            visf.close()
+            if step % 10 == 0:
+                whole_u = self.reassemble(u)
+                u_rhs_real = self.rhs(t, whole_u)
 
-        u = stepper(u, t, rhss)
+                visf = self.vis.make_file("fld-%04d" % step)
+                u_rhs = self.reassembled_rhs(t, u)
+                self.vis.add_data(visf, [ 
+                    ("u", whole_u), 
+                    ("u_rhs", u_rhs), 
+                    ("u_rhs_real", u_rhs_real), 
+                    ("rhsdiff", u_rhs_real-u_rhs), 
+                    ], 
+                    time=t, step=step)
+                visf.close()
 
-        if step % 100 == 0:
-            print la.norm(u[0]), la.norm(u[1])
+            u = self.stepper(u, t, self.rhss)
 
-    vis.close()
 
-    logmgr.tick()
-    logmgr.save()
+
+
+def main() :
+    tester = LocalDtTestRig()
+    tester.do_timestep()
+
+
 
 
 if __name__ == "__main__":
