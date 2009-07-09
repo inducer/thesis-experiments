776ee27696db3fea82e527829eb0a61d8af11065
Andreas Kloeckner <kloeckner@hpcgeek.ccv.brown.edu>
2009-07-08 22:45:49 -0400

    Fix parallel CUDA test runner.

--- hedge/maxwell/run-parallel-through-orders.sh
+++ hedge/maxwell/run-parallel-through-orders.sh
@@ -3,6 +3,8 @@
 
 ROOT=`dirname "$0"`
 
+BASE_DIR="$HOME/mpi-logs"
+
 for where in gpu cpu; do 
   for np in 16 12 8 4 1; do
     for o in `seq 1 9`; do 
@@ -13,16 +15,34 @@ for where in gpu cpu; do
       fi
 
       identifier="maxwell-np$np-o$o-$where"
-      complfile="$identifier-completed"
-      if ! (test -f "$complfile" && test -f "$identifier.dat-rank0"); then
+      complfile="$BASE_DIR/$identifier-completed"
+      logfile="$BASE_DIR/$identifier.dat"
+      if ! test -f "$logfile" && ! test -f "$logfile-rank0"; then
+        no_logfile=true
+      else
+        no_logfile=false
+      fi
+
+      if (! test -f "$complfile")  || (test "$no_logfile" = "true"); then
         echo "------------------------------------------------------"
         echo "ORDER $o $where NP $np"
         echo "------------------------------------------------------"
-        rm -f $identifier.dat*
+        if ! test -f "$complfile"; then
+          echo "rerunning because $complfile wasn't found"
+        fi
+        if test "$no_logfile" = "true"; then
+          echo "rerunning because $logfile or $logfile-rank0 wasn't found"
+        fi
+
+        echo "starting in 10 seconds..."
+        sleep 10 || exit
+        echo "deleting leftover state files..."
+        rm -f $BASE_DIR/$identifier.dat*
         rm -f "$complfile"
+        echo "starting..."
         ( mpirun -hostfile ~/hostfiles/hpcgeek-gpus -np $np \
           `which python` -O $ROOT/maxwell-cuda.py --single --local-watches \
-          --log-file="$identifier.dat" --mesh-size=$((np)) \
+          --log-file="$logfile" --mesh-size=$((np)) \
           --order=$o $HOPT $whereopt --steps=100 \
           "$@" && touch "$complfile")
       fi
