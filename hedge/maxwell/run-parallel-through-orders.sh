1228bfef36614336757807839e222d911dba973a
Andreas Kloeckner <kloeckner@hpcgeek.ccv.brown.edu>
2009-07-09 17:03:13 -0400

    CPU/GPU CUDA Maxwell test runner: add "mcpu" mode.

--- hedge/maxwell/run-parallel-through-orders.sh
+++ hedge/maxwell/run-parallel-through-orders.sh
@@ -4,12 +4,19 @@
 ROOT=`dirname "$0"`
 
 BASE_DIR="$HOME/mpi-logs"
+MCPU_FACTOR=4
 
-for where in gpu cpu; do 
-  for np in 16 12 8 4 1; do
+for where in gpu mcpu cpu ; do 
+  for loop_np in 16 12 8 4 1; do
     for o in `seq 1 9`; do 
+      mesh_size=$loop_np
+      np=$loop_np
+
       if test "$where" = "cpu"; then
         whereopt="--cpu"
+      elif test "$where" = "mcpu"; then
+        whereopt="--cpu"
+        np=$((np*MCPU_FACTOR))
       else
         whereopt="-d cuda_plan_no_progress"
       fi
@@ -42,8 +49,9 @@ for where in gpu cpu; do
         echo "starting..."
         ( mpirun -hostfile ~/hostfiles/hpcgeek-gpus -np $np \
           `which python` -O $ROOT/maxwell-cuda.py --single --local-watches \
-          --log-file="$logfile" --mesh-size=$((np)) \
+          --log-file="$logfile" --mesh-size=$mesh_size \
           --order=$o $HOPT $whereopt --steps=100 \
+          --extra-features=run_target:$where,mesh_size:$mesh_size,mcpu_factor:$MCPU_FACTOR \
           "$@" && touch "$complfile")
       fi
     done
