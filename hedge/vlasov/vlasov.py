8cc219848950c824710912d5f43a3889d801ba0c
Andreas Kloeckner <inform@tiker.net>
2009-07-10 17:33:08 -0400

    Make Vlasov-Maxwell execute (if not quite work...).

--- hedge/vlasov/vlasov.py
+++ hedge/vlasov/vlasov.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy
 import numpy.linalg as la
+from pytools import memoize_method
 
 
 
@@ -12,6 +13,8 @@ class VlasovOperatorBase:
         self.units = units
         self.species_mass = species_mass
 
+        self.v_dim = 1
+
         self.velocity_points = [units.v_from_p(species_mass, p)
                 for p in self.p_discr.quad_points]
 
@@ -20,14 +23,19 @@ class VlasovOperatorBase:
                 StrongAdvectionOperator(v, flux_type="upwind")
                 for v in self.velocity_points]
 
-    def op_template(self, forces, densities_base=0):
+    @memoize_method
+    def make_densities_placeholder(self, base_number=0):
+        from hedge.optemplate import make_vector_field
+        return make_vector_field("f",
+                range(base_number, 
+                    base_number+len(self.p_discr.quad_points)))
+
+    def op_template(self, forces_T, f=None):
         from hedge.optemplate import make_vector_field
         from hedge.tools import make_obj_array
 
-        f = make_vector_field("f",
-                range(densities_base, 
-                    densities_base
-                    +len(self.p_discr.quad_points)))
+        if f is None:
+            f = self.make_densities_placeholder()
 
         def adv_op_template(adv_op, f_of_p):
             from hedge.optemplate import Field, pair_with_boundary, \
@@ -56,10 +64,34 @@ class VlasovOperatorBase:
                     self.x_adv_operators)
                 ]) + sum(
                         forces_i*f_p_i
-                        for forces_i, f_p_i in zip(forces, f_p))
+                        for forces_i, f_p_i in zip(forces_T, f_p))
 
     def max_eigenvalue(self):
         return max(la.norm(v) for v in self.velocity_points)
+
+
+    def integral_dp(self, p_values):
+        return sum(qw_i * v_i 
+                for qw_i, v_i in zip(self.p_discr.quad_weights, values))
+
+    @memoize_method
+    def int_dv_quad_weights(self):
+        weights = numpy.zeros(self.p_discr.grid_size, dtype=numpy.float64)
+        c = self.units.VACUUM_LIGHT_SPEED
+        for i, p in enumerate(self.p_discr.quad_points):
+            p_square = numpy.dot(p, p)
+            denom = p_square + c**2 * self.species_mass**2
+            weights[i] =(
+                    c/denom**0.5
+                    - c*p_square / denom**1.5)
+
+        return weights*self.p_discr.quad_weights
+
+    def integral_dv(self, values):
+        return sum(qw_i * v_i 
+                for qw_i, v_i in zip(self.int_dv_quad_weights(), values))
+
+
 
 
 
@@ -76,11 +108,11 @@ class VlasovOperator(VlasovOperatorBase):
         force_base = Field("force")
 
         from hedge.tools import make_obj_array
-        forces = [make_obj_array([force_base[i][p_axis]
+        forces_T = [make_obj_array([force_base[i][p_axis]
             for i, p in enumerate(self.p_discr.quad_points)])
             for p_axis in [0]]
 
-        return VlasovOperatorBase.op_template(self, forces)
+        return VlasovOperatorBase.op_template(self, forces_T)
 
     def bind(self, discr):
         compiled_op_template = discr.compile(self.op_template())
@@ -94,32 +126,105 @@ class VlasovOperator(VlasovOperatorBase):
 
 
 
+
 class VlasovMaxwellOperator(VlasovOperatorBase):
-    def __init__(self, maxwell_op, *args, **kwarg):
-        VlasovOperator.__init__(self, *args, **kwargs)
+    def __init__(self, maxwell_op, units, species_mass, species_charge, 
+            *args, **kwargs):
+        VlasovOperatorBase.__init__(self, units, species_mass, *args, **kwargs)
 
         self.maxwell_op = maxwell_op
+        self.species_charge = species_charge
 
         from hedge.tools import count_subset
         self.maxwell_field_count = \
                 count_subset(self.maxwell_op.get_eh_subset())
 
     def op_template(self):
-        max_op_template = self.maxwell_op.op_template()
+        max_op = self.maxwell_op
 
-    def bind(discr):
-        bound_max_op = self.maxwell_op.bind(discr)
-        compiled_vlasov_op_template = \
-                discr.compile(self.op_template())
+        from hedge.optemplate import make_vector_field
+        max_fields = make_vector_field("w", self.maxwell_field_count)
+        max_e, max_h = max_op.split_eh(max_fields)
 
-        def rhs(t, eh_densities):
-            max_w = eh_densities[:self.maxwell_field_count]
-            densities = eh_densities[self.maxwell_field_count]
+        densities = self.make_densities_placeholder()
 
-            from hedge.tools import join_fields
-            return join_fields(
-                    bound_max_op(max_w),
-                    compiled_vlasov_op_template(f=densities))
+        j = self.integral_dv(
+                (self.species_charge*v)*f_i
+                for v, f_i in zip(self.velocity_points, densities))
+
+        # build forces --------------------------------------------------------
+        from hedge.optemplate import make_common_subexpression as cse
+        max_b = cse(max_op.mu * max_h)
+
+        v_subset = [True] * self.v_dim + [False] * (3-self.v_dim)
+        e_subset = max_op.get_eh_subset()[:3]
+        b_subset = max_op.get_eh_subset()[3:]
+        from hedge.tools import SubsettableCrossProduct
+        v_b_cross = SubsettableCrossProduct(
+                v_subset, b_subset, v_subset)
+
+        def pick_subset(tgt_subset, vec_subset, vec):
+            from hedge.tools import count_subset
+            result = numpy.zeros(count_subset(tgt_subset), 
+                    dtype = object)
+
+            tgt_idx = 0
+            for ts, vs, v_i in zip(tgt_subset, vec_subset, vec):
+                if ts:
+                    if vs:
+                        result[tgt_idx] = v_i
+                    else:
+                        result[tgt_idx] = 0
+                    tgt_idx += 1
+
+            return result
+
+        el_force = pick_subset(v_subset, e_subset,
+                cse(self.species_charge * max_e))
+
+        from hedge.tools import make_obj_array
+        q_times_b = cse(self.species_charge*max_b)
+        forces = make_obj_array([
+            el_force + v_b_cross(v, q_times_b)
+            for v in self.velocity_points])
+
+        forces_T = [[forces[v_node_idx][v_axis]
+            for v_node_idx in range(self.p_discr.grid_size)]
+            for v_axis in range(self.v_dim)]
+
+        # assemble rhs --------------------------------------------------------
+        max_e_rhs, max_h_rhs = max_op.split_eh(
+                max_op.op_template(max_fields))
+        vlasov_rhs = VlasovOperatorBase.op_template(
+                self, forces_T, densities)
+
+        from hedge.tools import join_fields
+        return join_fields(
+                max_e_rhs + j/max_op.epsilon,
+                max_h_rhs,
+                vlasov_rhs)
+
+    def bind(self, discr):
+        compiled = discr.compile(self.op_template())
+
+        def rhs(t, q):
+            max_w = q[:self.maxwell_field_count]
+            densities = q[self.maxwell_field_count:]
+
+            return compiled(w=max_w, f=densities)
+
+        return rhs
+
+    def split_e_h_densities(self, fields):
+        max_w = fields[:self.maxwell_field_count]
+        e, h = self.maxwell_op.split_eh(max_w)
+        densities = fields[self.maxwell_field_count:]
+        return e, h, densities
+
+    def max_eigenvalue(self):
+        return max(
+                max(la.norm(v) for v in self.velocity_points),
+                self.maxwell_op.max_eigenvalue())
 
 
 
@@ -173,7 +278,3 @@ def add_densities_to_silo(silo, vlasov_op, discr, densities):
     else:
         silo.put_quadvar1("f", "xpmesh", f_data, f_data.shape, 
                 DB_NODECENT)
-
-
-
-
---
+++ hedge/vlasov/vm-example.py
@@ -0,0 +1,114 @@
+from __future__ import division, with_statement
+
+
+
+
+def main():
+    from hedge.timestep import RK4TimeStepper
+    from hedge.tools import mem_checkpoint
+    from math import sin, cos, pi, sqrt, exp
+    from math import floor
+
+    from hedge.backends import guess_run_context
+    rcon = guess_run_context()
+
+    from hedge.mesh import make_uniform_1d_mesh
+    mesh = make_uniform_1d_mesh(-pi, pi, 20, periodic=True)
+
+    discr = rcon.make_discretization(mesh, order=4,
+            debug=[
+                "print_op_code",
+                "jit_dont_optimize_large_exprs",
+                ])
+
+    from hedge.visualization import SiloVisualizer
+    vis = SiloVisualizer(discr, rcon)
+
+    # operator setup ----------------------------------------------------------
+    from pyrticle.units import SI
+    units = SI()
+
+    from hedge.models.em import TE1DMaxwellOperator
+    max_op = TE1DMaxwellOperator(1, 1, flux_type=1,
+            dimensions=1)
+
+    from vlasov import VlasovMaxwellOperator
+    vlas_op = VlasovMaxwellOperator(max_op, units,
+            species_mass=1, species_charge=1,
+            grid_size=8, filter_type="exponential",
+            hard_scale=5, bounded_fraction=0.8,
+            filter_parameters=dict(eta_cutoff=0.3))
+
+    sine_vec = discr.interpolate_volume_function(lambda x, el: cos(0.5*x[0]))
+    from hedge.tools import make_obj_array, join_fields
+
+    densities = make_obj_array([
+        sine_vec.copy()*exp(-(0.5*v[0]**2))*v[0]
+        for v in vlas_op.p_discr.quad_points])
+
+    fields = join_fields(
+            max_op.assemble_eh(discr=discr),
+            densities)
+
+    # timestep setup ----------------------------------------------------------
+    stepper = RK4TimeStepper()
+
+    dt = discr.dt_factor(vlas_op.max_eigenvalue()) / 10
+    nsteps = int(10/dt)
+
+    print "%d elements, dt=%g, nsteps=%d" % (
+            len(discr.mesh.elements), dt, nsteps)
+
+    # diagnostics setup -------------------------------------------------------
+    from pytools.log import LogManager, \
+            add_general_quantities, \
+            add_simulation_quantities, \
+            add_run_info
+
+    logmgr = LogManager("vmax.dat", "w", rcon.communicator)
+    add_run_info(logmgr)
+    add_general_quantities(logmgr)
+    add_simulation_quantities(logmgr, dt)
+    discr.add_instrumentation(logmgr)
+
+    stepper.add_instrumentation(logmgr)
+
+    logmgr.add_watches(["step.max", "t_sim.max", "t_step.max"])
+
+    # timestep loop -----------------------------------------------------------
+    rhs = vlas_op.bind(discr)
+
+    from vlasov import add_densities_to_silo
+
+    try:
+        for step in xrange(nsteps):
+            logmgr.tick()
+
+            t = step*dt
+
+            if step % 20 == 0:
+                def real_part(fld):
+                    from hedge.tools import with_object_array_or_scalar
+                    return with_object_array_or_scalar(lambda x: x.real, fld)
+
+                with vis.make_file("vlasov-%04d" % step) as visf:
+                    e, h, densities = vlas_op.split_e_h_densities(fields)
+                    add_densities_to_silo(visf, vlas_op, discr, densities)
+                    vis.add_data(visf, [
+                        ("e", real_part(e)),
+                        ("h", real_part(h)),
+                        ], time=t, step=step)
+
+
+
+            fields = stepper(fields, t, dt, rhs)
+    finally:
+        logmgr.close()
+        discr.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
+
